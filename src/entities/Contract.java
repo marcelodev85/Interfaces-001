4a9dd4f368f40bc7c96c58e4979cbd2cbb6634c5
Marcelo <marceloantonio1985@gmail.com>
2023-07-19 22:46:19 -0300

    Adicionado classes de entidades e serviços

---
+++ src/entities/Contract.java
@@ -0,0 +1,47 @@
+package entities;
+
+import java.util.Date;
+
+public class Contract {
+	
+	private Integer number;
+	private Date date;
+	private double totalValue;
+	
+	public Contract() {
+		
+	}
+
+	public Contract(Integer number, Date date, double totalValue) {
+		super();
+		this.number = number;
+		this.date = date;
+		this.totalValue = totalValue;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(double totalValue) {
+		this.totalValue = totalValue;
+	}
+	
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author marcelo
+ *
+ */
+module Interfaces001 {
+}
